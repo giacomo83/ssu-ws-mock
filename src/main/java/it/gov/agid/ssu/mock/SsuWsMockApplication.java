74cf8d30e9273acb5865f96ccee74b0e55958b9c
jackom83 <giacomo.iacuaniello@gmail.com>
2020-02-12 10:43:37 +0100

    first commit

---
+++ src/main/java/it/gov/agid/ssu/mock/SsuWsMockApplication.java
@@ -0,0 +1,13 @@
+package it.gov.agid.ssu.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuWsMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuWsMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/agid/ssu/mock/SsuWsMockApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.agid.ssu.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuWsMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
